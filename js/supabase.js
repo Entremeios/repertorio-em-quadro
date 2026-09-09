69d6d799971184531a880f3c50c26359f877be67
Entremeios <wellingtonjnrs@gmail.com>
2026-09-09 01:33:47 -0300

    Refactor Supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -2,6 +2,6 @@ const SUPABASE_URL = "https://mfrfbuisatybyotdtdup.supabase.com";
 const SUPABASE_KEY = "sb_publishable_q2EPBiZQtxUiA9of61SONA_9OtU-Hrs";
 
 const supabaseClient = window.supabase.createClient(
-   https://mfrfbuisatybyotdtdup.supabase.com,
-    sb_publishable_q2EPBiZQtxUiA9of61SONA_9OtU-Hrs
+    SUPABASE_URL,
+    SUPABASE_KEY
 );

183d1a181ee0c51f227a5fc2d2cf7c615bd34110
Entremeios <wellingtonjnrs@gmail.com>
2026-09-09 00:46:10 -0300

    Initialize Supabase client with URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,14 @@
+/* =====================================================
+   SUPABASE
+   BIBLIOTECA VIRTUAL GONÇALVES DIAS
+===================================================== */
 
+const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO";
+
+const SUPABASE_KEY = "COLE_AQUI_SUA_PUBLISHABLE_KEY";
+
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);

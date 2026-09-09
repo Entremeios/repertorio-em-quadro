5e2ab429fa8d8221fcc1403ea943f0680af86393
Entremeios <wellingtonjnrs@gmail.com>
2026-09-09 01:32:18 -0300

    Fix syntax for supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -2,6 +2,6 @@ const SUPABASE_URL = "https://mfrfbuisatybyotdtdup.supabase.com";
 const SUPABASE_KEY = "sb_publishable_q2EPBiZQtxUiA9of61SONA_9OtU-Hrs";
 
 const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+   https://mfrfbuisatybyotdtdup.supabase.com,
+    sb_publishable_q2EPBiZQtxUiA9of61SONA_9OtU-Hrs
 );

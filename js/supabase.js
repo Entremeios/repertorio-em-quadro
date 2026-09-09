7ed1a2895821e0499ea9864fc9de000519c04fc8
Entremeios <wellingtonjnrs@gmail.com>
2026-09-09 01:29:14 -0300

    Remove unnecessary comments in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,5 @@
-/* =====================================================
-   SUPABASE
-   BIBLIOTECA VIRTUAL GONÇALVES DIAS
-===================================================== */
-
-const SUPABASE_URL = "https://mfrfbuisatybyotdtdup.supabase.com"
+const SUPABASE_URL = "https://mfrfbuisatybyotdtdup.supabase.com";
 const SUPABASE_KEY = "sb_publishable_q2EPBiZQtxUiA9of61SONA_9OtU-Hrs";
-
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
